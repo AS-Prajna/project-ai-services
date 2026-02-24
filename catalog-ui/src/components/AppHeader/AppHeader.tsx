0a31d38b257f8712d38b31be47aa367f8f3b92c4
AS-Prajna <a.s.prajna@ibm.com>
2026-02-24 13:10:42 +0000

    Initial changes for header and navbar
    Signed-off-by: AS-Prajna <a.s.prajna@ibm.com>

---
+++ catalog-ui/src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,49 @@
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  HeaderMenuButton,
+  Theme,
+} from "@carbon/react";
+import {
+  Help,
+  Notification,
+  User,
+} from "@carbon/icons-react";
+
+interface AppHeaderProps {
+  isSideNavOpen: boolean;
+  setIsSideNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppHeader = ({ isSideNavOpen, setIsSideNavOpen }: AppHeaderProps) => {
+  return (
+    
+    <Theme theme="g100">
+      <Header aria-label="IBM Power Operations Platform">
+         <HeaderMenuButton aria-label="Open menu" onClick={() => setIsSideNavOpen(!isSideNavOpen)} isActive={isSideNavOpen} isCollapsible />
+
+        <HeaderName prefix="IBM">
+          Power Operations Platform
+        </HeaderName>
+
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Help">
+            <Help size={20} />
+          </HeaderGlobalAction>
+
+          <HeaderGlobalAction aria-label="Notifications">
+            <Notification size={20} />
+          </HeaderGlobalAction>
+
+          <HeaderGlobalAction aria-label="User">
+            <User size={20} />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    </Theme>
+  );
+};
+
+export default AppHeader;
---
+++ catalog-ui/src/pages/Login/Login.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import AppHeader from "@/components/AppHeader";
+import Navbar from "@/components/Navbar";
+
+
+const Login = () => {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  return (
+    <>
+      <AppHeader
+        isSideNavOpen={isSideNavOpen}
+        setIsSideNavOpen={setIsSideNavOpen}
+      />
+
+      <Navbar isSideNavOpen={isSideNavOpen} />
+
+      <main
+        style={{
+          marginLeft: isSideNavOpen ? "256px" : "0",
+          padding: "2rem",
+          transition: "margin 0.2s ease",
+        }}
+      >
+     
+      </main>
+    </>
+  );
+};
+
+export default Login;
