0c539fedb9fd8672de168f898953d8344d943d70
AS-Prajna <a.s.prajna@ibm.com>
2026-05-06 11:30:39 +0000

    Reusable Service cards
    Signed-off-by: AS-Prajna <a.s.prajna@ibm.com>

--- catalog-ui/src/components/index.ts
+++ catalog-ui/src/components/index.ts
@@ -3,5 +3,7 @@ export { default as Navbar } from "./Navbar";
 export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as ArchitectureCard } from "./ArchitectureCard";
 export { default as SolutionCard } from "./SolutionCard";
+export { default as ServiceCard } from "./ServiceCard";
 export type { ArchitectureCardProps } from "./ArchitectureCard";
 export type { SolutionCardProps } from "./SolutionCard";
+export type { ServiceCardProps } from "./ServiceCard";
